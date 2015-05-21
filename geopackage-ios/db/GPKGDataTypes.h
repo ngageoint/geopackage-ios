0d060cfe4a6eff104e503e89475983ea37a7c489
osbornb <osbornb@bit-sys.com>
2015-05-21 12:43:52 -0600

    DB data types

--- geopackage-ios/db/GPKGDataTypes.h
+++ geopackage-ios/db/GPKGDataTypes.h
@@ -24,8 +24,24 @@ enum GPKGDataType{
     DATETIME
 };
 
+extern NSString * const DT_BOOLEAN;
+extern NSString * const DT_TINYINT;
+extern NSString * const DT_SMALLINT;
+extern NSString * const DT_MEDIUMINT;
+extern NSString * const DT_INT;
+extern NSString * const DT_INTEGER;
+extern NSString * const DT_FLOAT;
+extern NSString * const DT_DOUBLE;
+extern NSString * const DT_REAL;
+extern NSString * const DT_TEXT;
+extern NSString * const DT_BLOB;
+extern NSString * const DT_DATE;
+extern NSString * const DT_DATETIME;
+
 @interface GPKGDataTypes : NSObject
 
 +(NSString *) name: (enum GPKGDataType) dataType;
+
++(Class) classType: (enum GPKGDataType) dataType;
 
 @end
--- geopackage-ios/db/GPKGDataTypes.m
+++ geopackage-ios/db/GPKGDataTypes.m
@@ -8,11 +8,100 @@
 
 #import "GPKGDataTypes.h"
 
+NSString * const DT_BOOLEAN = @"BOOLEAN";
+NSString * const DT_TINYINT = @"TINYINT";
+NSString * const DT_SMALLINT = @"SMALLINT";
+NSString * const DT_MEDIUMINT = @"MEDIUMINT";
+NSString * const DT_INT = @"INT";
+NSString * const DT_INTEGER = @"INTEGER";
+NSString * const DT_FLOAT = @"FLOAT";
+NSString * const DT_DOUBLE = @"DOUBLE";
+NSString * const DT_REAL = @"REAL";
+NSString * const DT_TEXT = @"TEXT";
+NSString * const DT_BLOB = @"BLOB";
+NSString * const DT_DATE = @"DATE";
+NSString * const DT_DATETIME = @"DATETIME";
+
 @implementation GPKGDataTypes
 
 +(NSString *) name: (enum GPKGDataType) dataType{
-    // TODO
-    return nil;
+    NSString * name = nil;
+    
+    switch(dataType){
+        case BOOLEAN:
+            name = DT_BOOLEAN;
+            break;
+        case TINYINT:
+            name = DT_TINYINT;
+            break;
+        case SMALLINT:
+            name = DT_SMALLINT;
+            break;
+        case MEDIUMINT:
+            name = DT_MEDIUMINT;
+            break;
+        case INT:
+            name = DT_INT;
+            break;
+        case INTEGER:
+            name = DT_INTEGER;
+            break;
+        case FLOAT:
+            name = DT_FLOAT;
+            break;
+        case DOUBLE:
+            name = DT_DOUBLE;
+            break;
+        case REAL:
+            name = DT_REAL;
+            break;
+        case TEXT:
+            name = DT_TEXT;
+            break;
+        case BLOB:
+            name = DT_BLOB;
+            break;
+        case DATE:
+            name = DT_DATE;
+            break;
+        case DATETIME:
+            name = DT_DATETIME;
+            break;
+    }
+    
+    return name;
+}
+
++(Class) classType: (enum GPKGDataType) dataType{
+    Class class = nil;
+    
+    switch(dataType){
+        case BOOLEAN:
+        case TINYINT:
+        case SMALLINT:
+        case MEDIUMINT:
+        case INT:
+        case INTEGER:
+            class = [NSNumber class];
+            break;
+        case FLOAT:
+        case DOUBLE:
+        case REAL:
+            class = [NSDecimalNumber class];
+            break;
+        case TEXT:
+            class = [NSString class];
+            break;
+        case BLOB:
+            class = [NSData class];
+            break;
+        case DATE:
+        case DATETIME:
+            class = [NSDate class];
+            break;
+    }
+    
+    return class;
 }
 
 @end
