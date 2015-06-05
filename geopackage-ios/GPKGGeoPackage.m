f7d93b52b1e8925d8926da90bff34c11c33e6863
osbornb <osbornb@bit-sys.com>
2015-06-05 14:38:35 -0600

    geo package method implementations

--- geopackage-ios/GPKGGeoPackage.m
+++ geopackage-ios/GPKGGeoPackage.m
@@ -84,7 +84,49 @@
 -(GPKGGeometryColumns *) createFeatureTableWithGeometryColumns: (GPKGGeometryColumns *) geometryColumns
                                                 andBoundingBox: (GPKGBoundingBox *) boundingBox
                                                 andSrsId: (NSNumber *) srsId{
-    return nil; // TODO
+    
+    // Get the SRS
+    GPKGSpatialReferenceSystem * srs = [self getSrs:srsId];
+    
+    // Create the Geometry Columns table
+    [self createGeometryColumnsTable];
+    
+    // Create the user feature table
+    NSMutableArray * columns = [[NSMutableArray alloc] init];
+    [columns addObject:[GPKGFeatureColumn createPrimaryKeyColumnWithIndex:0 andName:@"id"]];
+    [columns addObject:[GPKGFeatureColumn createGeometryColumnWithIndex:1
+                                                                andName:geometryColumns.columnName
+                                                        andGeometryType:[geometryColumns getGeometryType]
+                                                             andNotNull:false
+                                                        andDefaultValue:nil]];
+    GPKGFeatureTable * table = [[GPKGFeatureTable alloc] initWithTable:geometryColumns.tableName andColumns:columns];
+    [self createFeatureTable:table];
+    
+    @try {
+        // Create the contents
+        GPKGContents * contents = [[GPKGContents alloc] init];
+        [contents setTableName:geometryColumns.tableName];
+        [contents setDataType:GPKG_CDT_FEATURES_NAME];
+        [contents setIdentifier:geometryColumns.tableName];
+        [contents setLastChange:[NSDate date]];
+        [contents setMinX:boundingBox.minLongitude];
+        [contents setMinY:boundingBox.minLatitude];
+        [contents setMaxX:boundingBox.maxLongitude];
+        [contents setMaxY:boundingBox.maxLatitude];
+        [contents setSrs:srs];
+        [[self getContentsDao] create:contents];
+        
+        // Create new geometry columns
+        [geometryColumns setContents:contents];
+        [geometryColumns setSrs:srs];
+        [[self getGeometryColumnsDao] create:geometryColumns];
+    }
+    @catch (NSException *e) {
+        [self deleteUserTableQuietly:geometryColumns.tableName];
+        @throw e;
+    }
+    
+    return geometryColumns;
 }
 
 -(GPKGTileMatrixSetDao *) getTileMatrixSetDao{
@@ -130,7 +172,52 @@
                                                 andContentsSrsId: (NSNumber *) contentsSrsId
                                                 andTileMatrixSetBoundingBox: (GPKGBoundingBox *) tileMatrixSetBoundingBox
                                                 andTileMatrixSetSrsId: (NSNumber *) tileMatrixSetSrsId{
-    return nil; // TODO
+    
+    GPKGTileMatrixSet * tileMatrixSet = nil;
+    
+    // Get the SRS
+    GPKGSpatialReferenceSystem * contentsSrs = [self getSrs:contentsSrsId];
+    GPKGSpatialReferenceSystem * tileMatrixSetSrs = [self getSrs:tileMatrixSetSrsId];
+    
+    // Create the Tile Matrix Set and Tile Matrix tables
+    [self createTileMatrixSetTable];
+    [self createTileMatrixTable];
+    
+    // Create the user tile table
+    NSArray * columns = nil; //TODO: [GPKGTileTable createRequiredColumns];
+    GPKGTileTable * table = [[GPKGTileTable alloc] initWithTable:tableName andColumns:columns];
+    [self createTileTable:table];
+    
+    @try {
+        // Create the contents
+        GPKGContents * contents = [[GPKGContents alloc] init];
+        [contents setTableName:tableName];
+        [contents setDataType:GPKG_CDT_TILES_NAME];
+        [contents setIdentifier:tableName];
+        [contents setLastChange:[NSDate date]];
+        [contents setMinX:contentsBoundingBox.minLongitude];
+        [contents setMinY:contentsBoundingBox.minLatitude];
+        [contents setMaxX:contentsBoundingBox.maxLongitude];
+        [contents setMaxY:contentsBoundingBox.maxLatitude];
+        [contents setSrs:contentsSrs];
+        [[self getContentsDao] create:contents];
+        
+        // Create new matrix tile set
+        tileMatrixSet = [[GPKGTileMatrixSet alloc] init];
+        [tileMatrixSet setContents:contents];
+        [tileMatrixSet setSrs:tileMatrixSetSrs];
+        [tileMatrixSet setMinX:tileMatrixSetBoundingBox.minLongitude];
+        [tileMatrixSet setMinY:tileMatrixSetBoundingBox.minLatitude];
+        [tileMatrixSet setMaxX:tileMatrixSetBoundingBox.maxLongitude];
+        [tileMatrixSet setMaxY:tileMatrixSetBoundingBox.maxLatitude];
+        [[self getTileMatrixSetDao] create:tileMatrixSet];
+    }
+    @catch (NSException *e) {
+        [self deleteUserTableQuietly:tableName];
+        @throw e;
+    }
+    
+    return tileMatrixSet;
 }
 
 -(GPKGDataColumnsDao *) getDataColumnsDao{
@@ -281,8 +368,31 @@
 }
 
 -(GPKGTileDao *) getTileDaoWithTableName: (NSString *) tableName{
-    //TODO
-    return nil;
+    
+    GPKGTileMatrixSetDao * dao = [self getTileMatrixSetDao];
+    
+    GPKGTileMatrixSet *tileMatrixSet = nil;
+    
+    GPKGResultSet * tileMatrixSetList = nil;
+    @try{
+        tileMatrixSetList = [dao queryForEqWithField:GPKG_TMS_COLUMN_TABLE_NAME andValue:tableName];
+        if([tileMatrixSetList moveToNext]){
+            tileMatrixSet = (GPKGTileMatrixSet *)[dao getObject: tileMatrixSetList];
+            if([tileMatrixSetList moveToNext]){
+                [NSException raise:@"Multiple Tile Matrix Sets" format:@"Unexpected state. More than one Tile Matrix Set matched for table name: %@, count: %d", tableName, [tileMatrixSetList count]];
+            }
+        }
+    }@finally{
+        if(tileMatrixSetList != nil){
+            [tileMatrixSetList close];
+        }
+    }
+    
+    if(tileMatrixSet == nil){
+        [NSException raise:@"No Tile Matrix Set" format:@"No Tile Table exists for table name: %@", tableName];
+    }
+    
+    return [self getTileDaoWithTileMatrixSet:tileMatrixSet];
 }
 
 -(void) dropSQLiteTriggers: (GPKGGeometryColumns *) geometryColumns{
@@ -295,6 +405,15 @@
         [self.database exec:[NSString stringWithFormat:@"DROP TRIGGER IF EXISTS rtree_%@_%@_update4", geometryColumns.tableName, geometryColumns.columnName]];
         [self.database exec:[NSString stringWithFormat:@"DROP TRIGGER IF EXISTS rtree_%@_%@_delete", geometryColumns.tableName, geometryColumns.columnName]];
     }
+}
+
+-(GPKGSpatialReferenceSystem *) getSrs: (NSNumber *) srsId{
+    GPKGSpatialReferenceSystemDao * dao = [self getSpatialReferenceSystemDao];
+    GPKGSpatialReferenceSystem * srs = (GPKGSpatialReferenceSystem *)[dao queryForIdObject:srsId];
+    if(srs == nil){
+        [NSException raise:@"No SRS" format:@"Spatial Reference System could not be found. SRS ID: %@", srsId];
+    }
+    return srs;
 }
 
 @end
