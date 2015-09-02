e297d1939b8df3ac7ad96a5a2b5a79dd83803ef3
osbornb <osbornb@bit-sys.com>
2015-09-02 10:12:31 -0600

    conversion to dynamic project

---
+++ geopackage-ios/geopackage_ios.h
@@ -0,0 +1,19 @@
+//
+//  geopackage-ios.h
+//  geopackage-ios
+//
+//  Created by Brian Osborn on 9/1/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for geopackage-ios.
+FOUNDATION_EXPORT double geopackage_iosVersionNumber;
+
+//! Project version string for geopackage-ios.
+FOUNDATION_EXPORT const unsigned char geopackage_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <geopackage_ios/PublicHeader.h>
+
+
