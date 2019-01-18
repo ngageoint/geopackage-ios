58b19f56a46a762161fc569a924aca1535d7bc1d
Brian Osborn <bosborn@users.noreply.github.com>
2019-01-18 15:26:36 -0700

    feature style extension in progress

--- geopackage-ios/extension/style/GPKGFeatureStyleExtension.h
+++ geopackage-ios/extension/style/GPKGFeatureStyleExtension.h
@@ -7,7 +7,233 @@
 //
 
 #import "GPKGBaseExtension.h"
+#import "GPKGRelatedTablesExtension.h"
+#import "GPKGContentsIdExtension.h"
 
+extern NSString * const GPKG_EXTENSION_FEATURE_STYLE_AUTHOR;
+extern NSString * const GPKG_EXTENSION_FEATURE_STYLE_NAME_NO_AUTHOR;
+extern NSString * const GPKG_PROP_EXTENSION_FEATURE_STYLE_DEFINITION;
+
+/**
+ * Table name prefix for mapping styles
+ */
+extern NSString * const GPKG_FSE_TABLE_MAPPING_STYLE;
+
+/**
+ * Table name prefix for mapping style defaults
+ */
+extern NSString * const GPKG_FSE_TABLE_MAPPING_TABLE_STYLE;
+
+/**
+ * Table name prefix for mapping icons
+ */
+extern NSString * const GPKG_FSE_TABLE_MAPPING_ICON;
+
+/**
+ * Table name prefix for mapping icon defaults
+ */
+extern NSString * const GPKG_FSE_TABLE_MAPPING_TABLE_ICON;
+
+/**
+ * Feature Style extension
+ */
 @interface GPKGFeatureStyleExtension : GPKGBaseExtension
+
+/**
+ *  Initialize
+ *
+ *  @param geoPackage GeoPackage
+ *  @param relatedTables related tables
+ *
+ *  @return new feature style extension
+ */
+-(instancetype) initWithGeoPackage: (GPKGGeoPackage *) geoPackage andRelatedTables: (GPKGRelatedTablesExtension *) relatedTables;
+
+/**
+ * Get the feature tables registered with the extension
+ *
+ * @return list of feature table names
+ */
+-(NSArray<NSString *> *) tables;
+
+/**
+ * Determine if the GeoPackage has the extension
+ *
+ * @return true if has extension
+ */
+-(BOOL) has;
+
+/**
+ * Determine if the GeoPackage has the extension for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ * @return true if has extension
+ */
+-(BOOL) hasWithTable: (NSString *) featureTable;
+
+/**
+ * Get the related tables extension
+ *
+ * @return related tables extension
+ */
+-(GPKGRelatedTablesExtension *) relatedTables;
+
+/**
+ * Get the contents id extension
+ *
+ * @return contents id extension
+ */
+-(GPKGContentsIdExtension *) contentsId;
+
+/**
+ * Create style, icon, table style, and table icon relationships for the
+ * feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) createRelationshipsWithTable: (NSString *) featureTable;
+
+/**
+ * Check if feature table has a style, icon, table style, or table icon
+ * relationships
+ *
+ * @param featureTable
+ *            feature table
+ * @return true if has a relationship
+ */
+-(BOOL) hasRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Create a style relationship for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) createStyleRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Determine if a style relationship exists for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ * @return true if relationship exists
+ */
+-(BOOL) hasStyleRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Create a feature table style relationship
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) createTableStyleRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Determine if a feature table style relationship exists
+ *
+ * @param featureTable
+ *            feature table
+ * @return true if relationship exists
+ */
+-(BOOL) hasTableStyleRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Create an icon relationship for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) createIconRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Determine if an icon relationship exists for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ * @return true if relationship exists
+ */
+-(BOOL) hasIconRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Create a feature table icon relationship
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) createTableIconRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Determine if a feature table icon relationship exists
+ *
+ * @param featureTable
+ *            feature table
+ * @return true if relationship exists
+ */
+-(BOOL) hasTableIconRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Get the mapping table name
+ *
+ * @param tablePrefix
+ *            table name prefix
+ * @param featureTable
+ *            feature table name
+ * @return mapping table name
+ */
+-(NSString *) mappingTableNameWithPrefix: (NSString *) tablePrefix andTable: (NSString *) featureTable;
+
+/**
+ * Delete the style and icon table and row relationships for all feature
+ * tables
+ */
+-(void) deleteRelationships;
+
+/**
+ * Delete the style and icon table and row relationships for the feature
+ * table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) deleteRelationshipsWithTable: (NSString *) featureTable;
+
+/**
+ * Delete a style relationship for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) deleteStyleRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Delete a table style relationship for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) deleteTableStyleRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Delete a icon relationship for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) deleteIconRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Delete a table icon relationship for the feature table
+ *
+ * @param featureTable
+ *            feature table
+ */
+-(void) deleteTableIconRelationshipWithTable: (NSString *) featureTable;
+
+/**
+ * Completely remove and delete the extension and all styles and icons
+ */
+-(void) removeExtension;
 
 @end
--- geopackage-ios/extension/style/GPKGFeatureStyleExtension.m
+++ geopackage-ios/extension/style/GPKGFeatureStyleExtension.m
@@ -7,7 +7,252 @@
 //
 
 #import "GPKGFeatureStyleExtension.h"
+#import "GPKGExtensions.h"
+#import "GPKGProperties.h"
+
+NSString * const GPKG_EXTENSION_FEATURE_STYLE_AUTHOR = @"nga";
+NSString * const GPKG_EXTENSION_FEATURE_STYLE_NAME_NO_AUTHOR = @"feature_style";
+NSString * const GPKG_PROP_EXTENSION_FEATURE_STYLE_DEFINITION = @"geopackage.extensions.feature_style";
+
+NSString * const GPKG_FSE_TABLE_MAPPING_STYLE = @"nga_style_";
+NSString * const GPKG_FSE_TABLE_MAPPING_TABLE_STYLE = @"nga_style_default_";
+NSString * const GPKG_FSE_TABLE_MAPPING_ICON = @"nga_icon_";
+NSString * const GPKG_FSE_TABLE_MAPPING_TABLE_ICON = @"nga_icon_default_";
+
+@interface GPKGFeatureStyleExtension ()
+
+@property (nonatomic, strong) NSString *extensionName;
+@property (nonatomic, strong) NSString *extensionDefinition;
+@property (nonatomic, strong) GPKGRelatedTablesExtension *relatedTables;
+@property (nonatomic, strong) GPKGContentsIdExtension *contentsId;
+
+@end
 
 @implementation GPKGFeatureStyleExtension
+
+-(instancetype) initWithGeoPackage: (GPKGGeoPackage *) geoPackage andRelatedTables: (GPKGRelatedTablesExtension *) relatedTables{
+    self = [super initWithGeoPackage:geoPackage];
+    if(self != nil){
+        self.extensionName = [GPKGExtensions buildExtensionNameWithAuthor:GPKG_EXTENSION_FEATURE_STYLE_AUTHOR andExtensionName:GPKG_EXTENSION_FEATURE_STYLE_NAME_NO_AUTHOR];
+        self.extensionDefinition = [GPKGProperties getValueOfProperty:GPKG_PROP_EXTENSION_FEATURE_STYLE_DEFINITION];
+        self.relatedTables = relatedTables;
+        self.contentsId = [[GPKGContentsIdExtension alloc] initWithGeoPackage:geoPackage];
+    }
+    return self;
+}
+
+/**
+ * Get or create the extension
+ *
+ * @param featureTable
+ *            feature table
+ * @return extension
+ */
+-(GPKGExtensions *) getOrCreateWithTable: (NSString *) featureTable{
+    
+    GPKGExtensions *extension = [self getOrCreateWithExtensionName:self.extensionName andTableName:featureTable andColumnName:nil andDefinition:self.extensionDefinition andScope:GPKG_EST_READ_WRITE];
+    
+    return extension;
+}
+
+-(NSArray<NSString *> *) tables{
+    return nil; // TODO
+}
+
+-(BOOL) has{
+    return NO; // TODO
+}
+
+-(BOOL) hasWithTable: (NSString *) featureTable{
+    return NO; // TODO
+}
+
+-(GPKGRelatedTablesExtension *) relatedTables{
+    return nil; // TODO
+}
+
+-(GPKGContentsIdExtension *) contentsId{
+    return nil; // TODO
+}
+
+-(void) createRelationshipsWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(BOOL) hasRelationshipWithTable: (NSString *) featureTable{
+    return NO; // TODO
+}
+
+-(void) createStyleRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(BOOL) hasStyleRelationshipWithTable: (NSString *) featureTable{
+    return NO; // TODO
+}
+
+-(void) createTableStyleRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(BOOL) hasTableStyleRelationshipWithTable: (NSString *) featureTable{
+    return NO; // TODO
+}
+
+-(void) createIconRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(BOOL) hasIconRelationshipWithTable: (NSString *) featureTable{
+    return NO; // TODO
+}
+
+-(void) createTableIconRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(BOOL) hasTableIconRelationshipWithTable: (NSString *) featureTable{
+    return NO; // TODO
+}
+
+-(NSString *) mappingTableNameWithPrefix: (NSString *) tablePrefix andTable: (NSString *) featureTable{
+    return nil; // TODO
+}
+
+/**
+ * Check if the style extension relationship between a feature table and
+ * style extension table exists
+ *
+ * @param mappingTableName
+ *            mapping table name
+ * @param featureTable
+ *            feature table name
+ * @param baseTable
+ *            base table name
+ * @param relatedTable
+ *            related table name
+ * @return true if relationship exists
+ */
+-(BOOL) hasStyleRelationshipWithMappingTable: (NSString *) mappingTableName andBaseTable: (NSString *) baseTable andRelatedTable: (NSString *) relatedTable{
+    
+    return NO; // TODO
+    /*
+    boolean has = false;
+    
+    try {
+        has = relatedTables.hasRelations(baseTable, relatedTable,
+                                         mappingTableName);
+    } catch (SQLException e) {
+        throw new GeoPackageException(
+                                      "Failed to check if Feature Style Relationship exists. Base Table: "
+                                      + baseTable + ", Related Table: " + relatedTable
+                                      + ", Mapping Table: " + mappingTableName, e);
+    }
+    
+    return has;
+     */
+}
+
+/**
+ * Create a style extension relationship between a feature table and style
+ * extension table
+ *
+ * @param mappingTableName
+ *            mapping table name
+ * @param featureTable
+ *            feature table name
+ * @param baseTable
+ *            base table name
+ * @param relatedTable
+ *            related table name
+ */
+-(void) createStyleRelationshipWithMappingTable: (NSString *) mappingTableName andFeatureTable: (NSString *) featureTable andBaseTable: (NSString *) baseTable andRelatedTable: (NSString *) relatedTable{
+    
+    // TODO
+    /*
+    if (!hasStyleRelationship(mappingTableName, baseTable, relatedTable)) {
+        
+        // Create the extension
+        getOrCreate(featureTable);
+        
+        if (baseTable.equals(ContentsId.TABLE_NAME)) {
+            if (!contentsId.has()) {
+                contentsId.getOrCreateExtension();
+            }
+        }
+        
+        StyleMappingTable mappingTable = new StyleMappingTable(
+                                                               mappingTableName);
+        
+        if (relatedTable.equals(StyleTable.TABLE_NAME)) {
+            relatedTables.addAttributesRelationship(baseTable,
+                                                    new StyleTable(), mappingTable);
+        } else {
+            relatedTables.addMediaRelationship(baseTable, new IconTable(),
+                                               mappingTable);
+        }
+    }
+     */
+    
+}
+
+-(void) deleteRelationships{
+    // TODO
+}
+
+-(void) deleteRelationshipsWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(void) deleteStyleRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(void) deleteTableStyleRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(void) deleteIconRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+-(void) deleteTableIconRelationshipWithTable: (NSString *) featureTable{
+    // TODO
+}
+
+/**
+ * Delete a style extension feature table relationship and the mapping table
+ *
+ * @param mappingTableName
+ *            mapping table name
+ * @param featureTable
+ *            feature table name
+ */
+-(void) deleteStyleRelationshipWithMappingTable: (NSString *) mappingTableName andFeatureTable: (NSString *) featureTable {
+    
+    // TODO
+    /*
+    relatedTables.removeRelationshipsWithMappingTable(mappingTableName);
+    
+    if (!hasRelationship(featureTable)) {
+        try {
+            if (extensionsDao.isTableExists()) {
+                extensionsDao.deleteByExtension(EXTENSION_NAME,
+                                                featureTable);
+            }
+        } catch (SQLException e) {
+            throw new GeoPackageException(
+                                          "Failed to delete Feature Style extension. GeoPackage: "
+                                          + geoPackage.getName() + ", Feature Table: "
+                                          + featureTable, e);
+        }
+    }
+     */
+    
+}
+
+-(void) removeExtension{
+    // TODO
+}
 
 @end
