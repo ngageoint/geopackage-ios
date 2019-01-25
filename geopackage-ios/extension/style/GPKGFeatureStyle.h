5e4f41f346c2faee921b9407024961b6023545bb
Brian Osborn <bosborn@users.noreply.github.com>
2019-01-25 15:08:19 -0700

    feature style

--- geopackage-ios/extension/style/GPKGFeatureStyle.h
+++ geopackage-ios/extension/style/GPKGFeatureStyle.h
@@ -7,7 +7,71 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GPKGStyleRow.h"
+#import "GPKGIconRow.h"
 
+/**
+ * Feature Style, including a style and icon, for a single feature geometry
+ */
 @interface GPKGFeatureStyle : NSObject
+
+/**
+ * Style
+ */
+@property (nonatomic, strong) GPKGStyleRow *style;
+
+/**
+ * Icon
+ */
+@property (nonatomic, strong) GPKGIconRow *icon;
+
+/**
+ * Initialize
+ *
+ * @return new feature style
+ */
+-(instancetype) init;
+
+/**
+ * Initialize
+ *
+ * @param style style row
+ *
+ * @return new feature style
+ */
+-(instancetype) initWithStyle: (GPKGStyleRow *) style;
+
+/**
+ * Initialize
+ *
+ * @param icon  icon row
+ *
+ * @return new feature style
+ */
+-(instancetype) initWithIcon: (GPKGIconRow *) icon;
+
+/**
+ * Initialize
+ *
+ * @param style style row
+ * @param icon  icon row
+ *
+ * @return new feature style
+ */
+-(instancetype) initWithStyle: (GPKGStyleRow *) style andIcon: (GPKGIconRow *) icon;
+
+/**
+ * Check if the feature style has a style row
+ *
+ * @return true if has style row
+ */
+-(BOOL) hasStyle;
+
+/**
+ * Check if the feature style has an icon row
+ *
+ * @return true if has icon row
+ */
+-(BOOL) hasIcon;
 
 @end
--- geopackage-ios/extension/style/GPKGFeatureStyle.m
+++ geopackage-ios/extension/style/GPKGFeatureStyle.m
@@ -10,4 +10,36 @@
 
 @implementation GPKGFeatureStyle
 
+-(instancetype) init{
+    self = [self initWithStyle:nil andIcon:nil];
+    return self;
+}
+
+-(instancetype) initWithStyle: (GPKGStyleRow *) style{
+    self = [self initWithStyle:style andIcon:nil];
+    return self;
+}
+
+-(instancetype) initWithIcon: (GPKGIconRow *) icon{
+    self = [self initWithStyle:nil andIcon:icon];
+    return self;
+}
+
+-(instancetype) initWithStyle: (GPKGStyleRow *) style andIcon: (GPKGIconRow *) icon{
+    self = [super init];
+    if(self != nil){
+        self.style = style;
+        self.icon = icon;
+    }
+    return self;
+}
+
+-(BOOL) hasStyle{
+    return self.style != nil;
+}
+
+-(BOOL) hasIcon{
+    return self.icon != nil;
+}
+
 @end
