17d5e98ab5683da89144eb946c1471ff699a5aef
Brian Osborn <bosborn@gmail.com>
2020-01-06 16:50:33 -0700

    user columns in progress

--- geopackage-ios/user/GPKGUserColumns.h
+++ geopackage-ios/user/GPKGUserColumns.h
@@ -7,8 +7,50 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GPKGUserColumn.h"
 
-@interface GPKGUserColumns : NSObject
+/**
+ * Abstract collection of columns from a user table, representing a full set of
+ * table columns or a subset from a query
+ */
+@interface GPKGUserColumns : NSObject <NSMutableCopying>
+
+/**
+ *  Table name, null when a pre-ordered subset of columns for a query
+ */
+@property (nonatomic, strong) NSString *tableName;
+
+/**
+ * Custom column specification flag (subset of table columns or different
+ * ordering)
+ */
+@property (nonatomic) BOOL custom;
+
+/**
+ *  Initialize
+ *
+ *  @param tableName table name
+ *  @param columns     columns
+ *  @param custom       custom column specification
+ *
+ *  @return new user table
+ */
+-(instancetype) initWithTable: (NSString *) tableName andColumns: (NSArray *) columns andCustom: (BOOL) custom;
+
+/**
+ * Initialize
+ *
+ * @param userColumns
+ *            user columns
+ *
+ *  @return new user table
+ */
+-(instancetype) initWithUserColumns: (GPKGUserColumns *) userColumns;
+
+/**
+ * Update the table columns
+ */
+-(void) updateColumns;
 
 // TODO
 
--- geopackage-ios/user/GPKGUserColumns.m
+++ geopackage-ios/user/GPKGUserColumns.m
@@ -8,8 +8,68 @@
 
 #import "GPKGUserColumns.h"
 
+@interface GPKGUserColumns()
+
+/**
+ *  Array of column names
+ */
+@property (nonatomic, strong) NSMutableArray<NSString *> *columnNames;
+
+/**
+ *  Array of columns
+ */
+@property (nonatomic, strong) NSMutableArray<GPKGUserColumn *> *columns;
+
+/**
+ *  Mapping between column names and their index
+ */
+@property (nonatomic, strong) NSMutableDictionary<NSString *, NSNumber *> *nameToIndex;
+
+/**
+ *  Primary key column index
+ */
+@property (nonatomic) int pkIndex;
+
+@end
+
 @implementation GPKGUserColumns
 
+-(instancetype) initWithTable: (NSString *) tableName andColumns: (NSArray *) columns andCustom: (BOOL) custom{
+    self = [super init];
+    if(self != nil){
+        self.tableName = tableName;
+        self.columns = [NSMutableArray arrayWithArray:columns];
+        self.custom = custom;
+        self.nameToIndex = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+
+-(instancetype) initWithUserColumns: (GPKGUserColumns *) userColumns{
+    self = [super init];
+    if(self != nil){
+        self.tableName = userColumns.tableName;
+        self.columnNames = [NSMutableArray arrayWithArray:userColumns.columnNames];
+        self.columns = [[NSMutableArray alloc] init];
+        for(GPKGUserColumn *column in userColumns.columns){
+            GPKGUserColumn *copiedColumn = [column mutableCopy];
+            [_columns addObject:copiedColumn];
+        }
+        self.nameToIndex = [NSMutableDictionary dictionaryWithDictionary:userColumns.nameToIndex];
+        self.pkIndex = userColumns.pkIndex;
+    }
+    return self;
+}
+
+-(void) updateColumns{
+    // TODO
+}
+
 // TODO
+
+-(id) mutableCopyWithZone: (NSZone *) zone{
+    GPKGUserColumns *userColumns = [[[self class] allocWithZone:zone] initWithUserColumns:self];
+    return userColumns;
+}
 
 @end
