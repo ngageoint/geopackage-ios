66f7d59aebe8dc99e592aec7bf3ea5bfb9def941
Brian Osborn <bosborn@gmail.com>
2020-01-07 09:51:58 -0700

    user columns implementation

--- geopackage-ios/user/GPKGUserColumns.h
+++ geopackage-ios/user/GPKGUserColumns.h
@@ -52,6 +52,212 @@
  */
 -(void) updateColumns;
 
-// TODO
+/**
+ *  Check for duplicate column names
+ *
+ *  @param index         index
+ *  @param previousIndex previous index
+ *  @param column        column
+ */
+-(void) duplicateCheckWithIndex: (int) index andPreviousIndex: (NSNumber *) previousIndex andColumn: (NSString *) column;
+
+/**
+ *  Check for the expected data type
+ *
+ *  @param expected expected data type
+ *  @param column   column
+ */
+-(void) typeCheckWithExpected: (enum GPKGDataType) expected andColumn: (GPKGUserColumn *) column;
+
+/**
+ *  Check for missing columns
+ *
+ *  @param index  index
+ *  @param column column
+ */
+-(void) missingCheckWithIndex: (NSNumber *) index andColumn: (NSString *) column;
+
+/**
+ *  Get the column index of the column name
+ *
+ *  @param columnName column name
+ *
+ *  @return index
+ */
+-(int) columnIndexWithColumnName: (NSString *) columnName;
+
+/**
+ *  Get the column index of the column name
+ *
+ *  @param columnName column name
+ *  @param required     column existence is required
+ *
+ *  @return index
+ */
+-(int) columnIndexWithColumnName: (NSString *) columnName andRequired: (BOOL) required;
+
+/**
+ * Get the array of column names
+ *
+ * @return column names
+ */
+-(NSArray<NSString *> *) columnNames;
+
+/**
+ *  Get the column name at the index
+ *
+ *  @param index index
+ *
+ *  @return column name
+ */
+-(NSString *) columnNameWithIndex: (int) index;
+
+/**
+ * Get the list of columns
+ *
+ * @return columns
+ */
+-(NSArray<GPKGUserColumn *> *) columns;
+
+/**
+ *  Get the column at the index
+ *
+ *  @param index index
+ *
+ *  @return column
+ */
+-(GPKGUserColumn *) columnWithIndex: (int) index;
+
+/**
+ *  Get the column of the column name
+ *
+ *  @param columnName column name
+ *
+ *  @return column
+ */
+-(GPKGUserColumn *) columnWithColumnName: (NSString *) columnName;
+
+/**
+ * Check if the table has the column
+ *
+ * @param columnName
+ *            column name
+ * @return true if has the column
+ */
+-(BOOL) hasColumnWithColumnName: (NSString *) columnName;
+
+/**
+ *  Get the column count
+ *
+ *  @return column count
+ */
+-(int) columnCount;
+
+/**
+ * Check if the table has a primary key column
+ *
+ * @return true if has a primary key
+ */
+-(BOOL) hasPkColumn;
+
+/**
+ * Get the primary key column index
+ *
+ * @return primary key column index
+ */
+-(int) pkIndex;
+
+/**
+ *  Get the primary key column index
+ *
+ *  @return pk index
+ */
+-(GPKGUserColumn *) pkColumn;
+
+/**
+ *  Get the primary key column name
+ *
+ *  @return primary key column name
+ */
+-(NSString *) pkColumnName;
+
+/**
+ * Get the columns with the provided data type
+ *
+ * @param type
+ *            data type
+ * @return columns
+ */
+-(NSArray *) columnsOfType: (enum GPKGDataType) type;
+
+/**
+ * Add a new column
+ *
+ * @param column
+ *            new column
+ */
+-(void) addColumn: (GPKGUserColumn *) column;
+
+/**
+ * Rename a column
+ *
+ * @param column
+ *            column
+ * @param newColumnName
+ *            new column name
+ */
+-(void) renameColumn: (GPKGUserColumn *) column toColumn: (NSString *) newColumnName;
+
+/**
+ * Rename a column
+ *
+ * @param columnName
+ *            column name
+ * @param newColumnName
+ *            new column name
+ */
+-(void) renameColumnWithName: (NSString *) columnName toColumn: (NSString *) newColumnName;
+
+/**
+ * Rename a column
+ *
+ * @param index
+ *            column index
+ * @param newColumnName
+ *            new column name
+ */
+-(void) renameColumnWithIndex: (int) index toColumn: (NSString *) newColumnName;
+
+/**
+ * Drop a column
+ *
+ * @param column
+ *            column to drop
+ */
+-(void) dropColumn: (GPKGUserColumn *) column;
+
+/**
+ * Drop a column
+ *
+ * @param columnName
+ *            column name
+ */
+-(void) dropColumnWithName: (NSString *) columnName;
+
+/**
+ * Drop a column
+ *
+ * @param index
+ *            column index
+ */
+-(void) dropColumnWithIndex: (int) index;
+
+/**
+ * Alter a column
+ *
+ * @param column
+ *            altered column
+ */
+-(void) alterColumn: (GPKGUserColumn *) column;
 
 @end
--- geopackage-ios/user/GPKGUserColumns.m
+++ geopackage-ios/user/GPKGUserColumns.m
@@ -7,6 +7,7 @@
 //
 
 #import "GPKGUserColumns.h"
+#import "GPKGUtils.h"
 
 @interface GPKGUserColumns()
 
@@ -62,10 +63,229 @@
 }
 
 -(void) updateColumns{
-    // TODO
+    
+    [self.nameToIndex removeAllObjects];
+    
+    if(!self.custom){
+    
+        NSMutableSet<NSNumber *> *indices = [[NSMutableSet alloc] init];
+        
+        // Check for missing indices and duplicates
+        NSMutableArray<GPKGUserColumn *> *needsIndex = [[NSMutableArray alloc] init];
+        for (GPKGUserColumn *column in self.columns) {
+            if ([column hasIndex]) {
+                NSNumber *index = [NSNumber numberWithInt:column.index];
+                if([indices containsObject:index]){
+                    [NSException raise:@"Duplicate Index" format:@"Duplicate index: %@, Table Name: %@", index, self.tableName];
+                }else{
+                    [indices addObject:index];
+                }
+            } else {
+                [needsIndex addObject:column];
+            }
+        }
+        
+        // Update columns that need an index
+        int currentIndex = -1;
+        for (GPKGUserColumn *column in needsIndex) {
+            while([indices containsObject:[NSNumber numberWithInt:++currentIndex]]){
+            }
+            [column setIndex:currentIndex];
+        }
+        
+        // Sort the columns by index
+        NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"index" ascending:true];
+        self.columns = [NSMutableArray arrayWithArray:[self.columns sortedArrayUsingDescriptors:@[sort]]];
+    
+    }
+        
+    self.pkIndex = -1;
+    self.columnNames = [[NSMutableArray alloc] initWithCapacity:self.columns.count];
+    
+    for (int index = 0; index < self.columns.count; index++) {
+        
+        GPKGUserColumn *column = [self.columns objectAtIndex:index];
+        NSString *columnName = column.name;
+        NSString * lowerCaseColumnName = [columnName lowercaseString];
+        
+        if(!self.custom){
+        
+            if(column.index != index){
+                [NSException raise:@"No Column" format:@"No column found at index: %d, Table Name: %@", index, self.tableName];
+            }
+            
+            if([self.nameToIndex objectForKey:lowerCaseColumnName]){
+                [NSException raise:@"Duplicate Column" format:@"Duplicate column found at index: %d, Table Name: %@, Name: %@", index, self.tableName, columnName];
+            }
+            
+        }
+        
+        if(column.primaryKey){
+            if(self.pkIndex != -1){
+                NSMutableString *error = [NSMutableString stringWithString:@"More than one primary key column was found for "];
+                if(self.custom){
+                    [error appendString:@"custom specified table columns"];
+                }else{
+                    [error appendString:@"table"];
+                }
+                [error appendFormat:@". table: %@, index1: %d, index2: %d", self.tableName, self.pkIndex, index];
+                if(self.custom){
+                    [error appendFormat:@", columns: [%@]", [self.columnNames componentsJoinedByString:@", "]];
+                }
+                [NSException raise:@"Multiple Primary Keys" format:error, nil];
+            }
+            self.pkIndex = index;
+        }
+        
+        [_columnNames addObject:columnName];
+        [self.nameToIndex setObject:[NSNumber numberWithInt:index] forKey:lowerCaseColumnName];
+    }
+    
 }
 
-// TODO
+-(void) duplicateCheckWithIndex: (int) index andPreviousIndex: (NSNumber *) previousIndex andColumn: (NSString *) column{
+    if(previousIndex != nil){
+        [NSException raise:@"Duplicate Column" format:@"More than one %@ column was found for table '%@'. Index %@ and %d", column, self.tableName, previousIndex, index];
+    }
+}
+
+-(void) typeCheckWithExpected: (enum GPKGDataType) expected andColumn: (GPKGUserColumn *) column{
+    enum GPKGDataType actual = column.dataType;
+    if(actual != expected){
+        [NSException raise:@"Unexpected Data Type" format:@"Unexpected %@ column data type was found for table '%@', expected: %@, actual: %@", column.name, self.tableName, [GPKGDataTypes name:expected], column.type];
+    }
+}
+
+-(void) missingCheckWithIndex: (NSNumber *) index andColumn: (NSString *) column{
+    if(index == nil){
+        [NSException raise:@"Missing Column" format:@"No %@ column was found for table '%@'", column, self.tableName];
+    }
+}
+
+-(int) columnIndexWithColumnName: (NSString *) columnName{
+    return [self columnIndexWithColumnName:columnName andRequired:YES];
+}
+
+-(int) columnIndexWithColumnName: (NSString *) columnName andRequired: (BOOL) required{
+    NSNumber *index = [GPKGUtils objectForKey:[columnName lowercaseString] inDictionary:self.nameToIndex];
+    if(required && index == nil){
+        NSMutableString *error = [NSMutableString stringWithString:@"Column does not exist in "];
+        if(self.custom){
+            [error appendString:@"custom specified table columns"];
+        }else{
+            [error appendString:@"table"];
+        }
+        [error appendFormat:@". table: %@, column: %@", self.tableName, columnName];
+        if(self.custom){
+            [error appendFormat:@", columns: [%@]", [self.columnNames componentsJoinedByString:@", "]];
+        }
+        [NSException raise:@"No Column" format:error, nil];
+    }
+    return [index intValue];
+}
+
+-(NSArray<NSString *> *) columnNames{
+    return _columnNames;
+}
+
+-(NSString *) columnNameWithIndex: (int) index{
+    return [GPKGUtils objectAtIndex:index inArray:self.columnNames];
+}
+
+-(NSArray<GPKGUserColumn *> *) columns{
+    return _columns;
+}
+
+-(GPKGUserColumn *) columnWithIndex: (int) index{
+    return [GPKGUtils objectAtIndex:index inArray:self.columns];
+}
+
+-(GPKGUserColumn *) columnWithColumnName: (NSString *) columnName{
+    return [self columnWithIndex:[self columnIndexWithColumnName:columnName]];
+}
+
+-(BOOL) hasColumnWithColumnName: (NSString *) columnName{
+    return [self.nameToIndex objectForKey:[columnName lowercaseString]] != nil;
+}
+
+-(int) columnCount{
+    return (int)[self.columns count];
+}
+
+-(BOOL) hasPkColumn{
+    return self.pkIndex >= 0;
+}
+
+-(int) pkIndex{
+    return _pkIndex;
+}
+
+-(GPKGUserColumn *) pkColumn{
+    GPKGUserColumn * column = nil;
+    if([self hasPkColumn]){
+        column = [GPKGUtils objectAtIndex:self.pkIndex inArray:self.columns];
+    }
+    return column;
+}
+
+-(NSString *) pkColumnName{
+    return [self pkColumn].name;
+}
+
+-(NSArray *) columnsOfType: (enum GPKGDataType) type{
+    NSMutableArray * columnsOfType = [[NSMutableArray alloc] init];
+    for(GPKGUserColumn *column in self.columns){
+        if(column.dataType == type){
+            [columnsOfType addObject:column];
+        }
+    }
+    return columnsOfType;
+}
+
+-(void) addColumn: (GPKGUserColumn *) column{
+    [_columns addObject:column];
+    [self updateColumns];
+}
+
+-(void) addColumnWithoutUpdate: (GPKGUserColumn *) column{
+    [_columns addObject:column];
+}
+
+-(void) renameColumn: (GPKGUserColumn *) column toColumn: (NSString *) newColumnName{
+    [self renameColumnWithName:column.name toColumn:newColumnName];
+    [column setName:newColumnName];
+}
+
+-(void) renameColumnWithName: (NSString *) columnName toColumn: (NSString *) newColumnName{
+    [self renameColumnWithIndex:[self columnIndexWithColumnName:columnName] toColumn:newColumnName];
+}
+
+-(void) renameColumnWithIndex: (int) index toColumn: (NSString *) newColumnName{
+    [[_columns  objectAtIndex:index] setName:newColumnName];
+    [self updateColumns];
+}
+
+-(void) dropColumn: (GPKGUserColumn *) column{
+    [self dropColumnWithIndex:column.index];
+}
+
+-(void) dropColumnWithName: (NSString *) columnName{
+    [self dropColumnWithIndex:[self columnIndexWithColumnName:columnName]];
+}
+
+-(void) dropColumnWithIndex: (int) index{
+    [_columns removeObjectAtIndex:index];
+    for(GPKGUserColumn *column in _columns){
+        [column resetIndex];
+    }
+    [self updateColumns];
+}
+
+-(void) alterColumn: (GPKGUserColumn *) column{
+    GPKGUserColumn *existingColumn = [self columnWithColumnName:column.name];
+    [column setIndex:existingColumn.index];
+    [_columns replaceObjectAtIndex:column.index withObject:column];
+}
 
 -(id) mutableCopyWithZone: (NSZone *) zone{
     GPKGUserColumns *userColumns = [[[self class] allocWithZone:zone] initWithUserColumns:self];
