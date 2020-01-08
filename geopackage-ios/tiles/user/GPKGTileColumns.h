2c90031c5b2c4cb31ddf7cadc27409d136d01778
Brian Osborn <bosborn@gmail.com>
2020-01-08 13:57:56 -0700

    tile columns

--- geopackage-ios/tiles/user/GPKGTileColumns.h
+++ geopackage-ios/tiles/user/GPKGTileColumns.h
@@ -7,9 +7,127 @@
 //
 
 #import "GPKGUserColumns.h"
+#import "GPKGTileColumn.h"
 
+/**
+ *  Tile Table constants
+ */
+extern NSString * const GPKG_TC_COLUMN_ID;
+extern NSString * const GPKG_TC_COLUMN_ZOOM_LEVEL;
+extern NSString * const GPKG_TC_COLUMN_TILE_COLUMN;
+extern NSString * const GPKG_TC_COLUMN_TILE_ROW;
+extern NSString * const GPKG_TC_COLUMN_TILE_DATA;
+
+/**
+ * Collection of tile columns
+ */
 @interface GPKGTileColumns : GPKGUserColumns
 
-// TODO
+/**
+ *  Zoom level column index
+ */
+@property (nonatomic) int zoomLevelIndex;
+
+/**
+ *  Tile column column index
+ */
+@property (nonatomic) int tileColumnIndex;
+
+/**
+ *  Tile row column index
+ */
+@property (nonatomic) int tileRowIndex;
+
+/**
+ *  Tile data column index
+ */
+@property (nonatomic) int tileDataIndex;
+
+/**
+ *  Initialize
+ *
+ *  @param tableName table name
+ *  @param columns     columns
+ *
+ *  @return new tile columns
+ */
+-(instancetype) initWithTable: (NSString *) tableName andColumns: (NSArray *) columns;
+
+/**
+ *  Initialize
+ *
+ *  @param tableName table name
+ *  @param columns     columns
+ *  @param custom       custom column specification
+ *
+ *  @return new tile columns
+ */
+-(instancetype) initWithTable: (NSString *) tableName andColumns: (NSArray *) columns andCustom: (BOOL) custom;
+
+/**
+ * Initialize
+ *
+ * @param tileColumns
+ *            tile columns
+ *
+ *  @return new tile columns
+ */
+-(instancetype) initWithTileColumns: (GPKGTileColumns *) tileColumns;
+
+/**
+ *  Check if has a zoom level column
+ *
+ *  @return true if has a zoom level column
+ */
+-(BOOL) hasZoomLevelColumn;
+
+/**
+ *  Get the zoom level column
+ *
+ *  @return zoom level tile column
+ */
+-(GPKGTileColumn *) zoomLevelColumn;
+
+/**
+ *  Check if has a tile column column
+ *
+ *  @return true if has a tile column column
+ */
+-(BOOL) hasTileColumnColumn;
+
+/**
+ *  Get the tile column column
+ *
+ *  @return tile column column
+ */
+-(GPKGTileColumn *) tileColumnColumn;
+
+/**
+ *  Check if has a tile row column
+ *
+ *  @return true if has a tile row column
+ */
+-(BOOL) hasTileRowColumn;
+
+/**
+ *  Get the tile row column
+ *
+ *  @return tile row column
+ */
+-(GPKGTileColumn *) tileRowColumn;
+
+/**
+ *  Check if has a tile data column
+ *
+ *  @return true if has a tile data column
+ */
+-(BOOL) hasTileDataColumn;
+
+/**
+ *  Get the tile data column
+ *
+ *  @return tile data column
+ */
+-(GPKGTileColumn *) tileDataColumn;
 
 @end
--- geopackage-ios/tiles/user/GPKGTileColumns.m
+++ geopackage-ios/tiles/user/GPKGTileColumns.m
@@ -8,8 +8,139 @@
 
 #import "GPKGTileColumns.h"
 
+NSString * const GPKG_TC_COLUMN_ID = @"id";
+NSString * const GPKG_TC_COLUMN_ZOOM_LEVEL = @"zoom_level";
+NSString * const GPKG_TC_COLUMN_TILE_COLUMN = @"tile_column";
+NSString * const GPKG_TC_COLUMN_TILE_ROW = @"tile_row";
+NSString * const GPKG_TC_COLUMN_TILE_DATA = @"tile_data";
+
 @implementation GPKGTileColumns
 
-// TODO
+-(instancetype) initWithTable: (NSString *) tableName andColumns: (NSArray *) columns{
+    return [self initWithTable:tableName andColumns:columns andCustom:NO];
+}
+
+-(instancetype) initWithTable: (NSString *) tableName andColumns: (NSArray *) columns andCustom: (BOOL) custom{
+    self = [super initWithTable:tableName andColumns:columns andCustom:custom];
+    if(self != nil){
+        _zoomLevelIndex = -1;
+        _tileColumnIndex = -1;
+        _tileRowIndex = -1;
+        _tileDataIndex = -1;
+        [self updateColumns];
+    }
+    return self;
+}
+
+-(instancetype) initWithTileColumns: (GPKGTileColumns *) tileColumns{
+    self = [super initWithUserColumns:tileColumns];
+    if(self != nil){
+        _zoomLevelIndex = tileColumns.zoomLevelIndex;
+        _tileColumnIndex = tileColumns.tileColumnIndex;
+        _tileRowIndex = tileColumns.tileRowIndex;
+        _tileDataIndex = tileColumns.tileDataIndex;
+    }
+    return self;
+}
+
+-(void) updateColumns{
+    [super updateColumns];
+    
+    // Find the required columns
+    
+    NSNumber *zoomLevel = [self columnIndexWithColumnName:GPKG_TC_COLUMN_ZOOM_LEVEL andRequired:NO];
+    if(!self.custom){
+        [self missingCheckWithIndex:zoomLevel andColumn:GPKG_TC_COLUMN_ZOOM_LEVEL];
+    }
+    if(zoomLevel != nil){
+        int zoomLevelIndex = [zoomLevel intValue];
+        [self typeCheckWithExpected:GPKG_DT_INTEGER andColumn:[self columnWithIndex:zoomLevelIndex]];
+        _zoomLevelIndex = zoomLevelIndex;
+    }
+    
+    NSNumber *tileColumn = [self columnIndexWithColumnName:GPKG_TC_COLUMN_TILE_COLUMN andRequired:NO];
+    if(!self.custom){
+        [self missingCheckWithIndex:tileColumn andColumn:GPKG_TC_COLUMN_TILE_COLUMN];
+    }
+    if(tileColumn != nil){
+        int tileColumnIndex = [tileColumn intValue];
+        [self typeCheckWithExpected:GPKG_DT_INTEGER andColumn:[self columnWithIndex:tileColumnIndex]];
+        _tileColumnIndex = tileColumnIndex;
+    }
+    
+    NSNumber *tileRow = [self columnIndexWithColumnName:GPKG_TC_COLUMN_TILE_ROW andRequired:NO];
+    if(!self.custom){
+        [self missingCheckWithIndex:tileRow andColumn:GPKG_TC_COLUMN_TILE_ROW];
+    }
+    if(tileRow != nil){
+        int tileRowIndex = [tileRow intValue];
+        [self typeCheckWithExpected:GPKG_DT_INTEGER andColumn:[self columnWithIndex:tileRowIndex]];
+        _tileRowIndex = tileRowIndex;
+    }
+    
+    NSNumber *tileData = [self columnIndexWithColumnName:GPKG_TC_COLUMN_TILE_DATA andRequired:NO];
+    if(!self.custom){
+        [self missingCheckWithIndex:tileData andColumn:GPKG_TC_COLUMN_TILE_DATA];
+    }
+    if(tileData != nil){
+        int tileDataIndex = [tileData intValue];
+        [self typeCheckWithExpected:GPKG_DT_BLOB andColumn:[self columnWithIndex:tileDataIndex]];
+        _tileDataIndex = tileDataIndex;
+    }
+    
+}
+
+-(BOOL) hasZoomLevelColumn{
+    return self.zoomLevelIndex >= 0;
+}
+
+-(GPKGTileColumn *) zoomLevelColumn{
+    GPKGTileColumn *column = nil;
+    if([self hasZoomLevelColumn]){
+        column = (GPKGTileColumn *) [self columnWithIndex:self.zoomLevelIndex];
+    }
+    return column;
+}
+
+-(BOOL) hasTileColumnColumn{
+    return self.tileColumnIndex >= 0;
+}
+
+-(GPKGTileColumn *) tileColumnColumn{
+    GPKGTileColumn *column = nil;
+    if([self hasTileColumnColumn]){
+        column = (GPKGTileColumn *) [self columnWithIndex:self.tileColumnIndex];
+    }
+    return column;
+}
+
+-(BOOL) hasTileRowColumn{
+    return self.tileRowIndex >= 0;
+}
+
+-(GPKGTileColumn *) tileRowColumn{
+    GPKGTileColumn *column = nil;
+    if([self hasTileRowColumn]){
+        column = (GPKGTileColumn *) [self columnWithIndex:self.tileRowIndex];
+    }
+    return column;
+}
+
+-(BOOL) hasTileDataColumn{
+    return self.tileDataIndex >= 0;
+}
+
+-(GPKGTileColumn *) tileDataColumn{
+    GPKGTileColumn *column = nil;
+    if([self hasTileDataColumn]){
+        column = (GPKGTileColumn *) [self columnWithIndex:self.tileDataIndex];
+    }
+    return column;
+}
+
+-(id) mutableCopyWithZone: (NSZone *) zone{
+    GPKGTileColumns *tileColumns = [super mutableCopyWithZone:zone];
+    return tileColumns;
+}
 
 @end
