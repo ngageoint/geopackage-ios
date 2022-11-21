a0eddf294edae0fb338b412eaa6d52756dab85e1
Brian Osborn <bosborn@gmail.com>
2022-11-21 10:19:00 -0700

    dgiwg updates

--- geopackage-ios/dgiwg/GPKGDgiwgGeoPackageManager.h
+++ geopackage-ios/dgiwg/GPKGDgiwgGeoPackageManager.h
@@ -7,13 +7,111 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GPKGGeoPackageManager.h"
+#import "GPKGDgiwgFile.h"
+#import "GPKGDgiwgGeoPackage.h"
 
 /**
  * DGIWG (Defence Geospatial Information Working Group) GeoPackage Manager used
  * to create and open GeoPackages
  */
-@interface GPKGDgiwgGeoPackageManager : NSObject
+@interface GPKGDgiwgGeoPackageManager : GPKGGeoPackageManager
 
-// TODO
+/**
+ * Initialize
+ *
+ * @return new DGIWG GeoPackage Manager
+ */
+-(instancetype) init;
+
+/**
+ * Create a GeoPackage
+ *
+ * @param database database name
+ * @param metadata metadata
+ * @return GeoPackage file if created
+ */
+-(GPKGDgiwgFile *) create: (NSString *) database withMetadata: (NSString *) metadata;
+
+/**
+ * Create a GeoPackage
+ *
+ * @param database database name
+ * @param uri      URI
+ * @param metadata metadata
+ * @return GeoPackage file if created
+ */
+-(GPKGDgiwgFile *) create: (NSString *) database withURI: (NSString *) uri andMetadata: (NSString *) metadata;
+
+/**
+ *  Create a new GeoPackage database in specified directory
+ *
+ *  @param database    database name
+ *  @param dbDirectory directory
+ *  @param metadata metadata
+ *  @return GeoPackage file if created
+ */
+-(GPKGDgiwgFile *) create: (NSString *) database inDirectory: (NSString *) dbDirectory withMetadata: (NSString *) metadata;
+
+/**
+ *  Create a new GeoPackage database in specified directory
+ *
+ *  @param database    database name
+ *  @param dbDirectory directory
+ *  @param uri      URI
+ *  @param metadata metadata
+ *  @return GeoPackage file if created
+ */
+-(GPKGDgiwgFile *) create: (NSString *) database inDirectory: (NSString *) dbDirectory withURI: (NSString *) uri andMetadata: (NSString *) metadata;
+
+/**
+ * Open the database
+ *
+ * @param database database name
+ * @return open GeoPackage
+ */
+-(GPKGDgiwgGeoPackage *) open: (NSString *) database;
+
+/**
+ * Open the database
+ *
+ * @param database database name
+ * @param validate validate the GeoPackage
+ * @return open GeoPackage
+ */
+-(GPKGDgiwgGeoPackage *) open: (NSString *) database andValidate: (BOOL) validate;
+
+/**
+ * Delete the database
+ *
+ * @param file GeoPackage file
+ * @return true if deleted
+ */
+-(BOOL) deleteDGIWG: (GPKGDgiwgFile *) file;
+
+/**
+ * Delete the database
+ *
+ * @param file GeoPackage file
+ * @param deleteFile true to delete the GeoPackage file
+ * @return true if deleted
+ */
+-(BOOL) deleteDGIWG: (GPKGDgiwgFile *) file andFile: (BOOL) deleteFile;
+
+/**
+ * Is the GeoPackage valid according to the DGIWG GeoPackage Profile
+ *
+ * @param geoPackage GeoPackage
+ * @return true if valid
+ */
++(BOOL) isValid: (GPKGDgiwgGeoPackage *) geoPackage;
+
+/**
+ * Validate the GeoPackage against the DGIWG GeoPackage Profile
+ *
+ * @param geoPackage GeoPackage
+ * @return validation errors
+ */
++(GPKGDgiwgValidationErrors *) validate: (GPKGDgiwgGeoPackage *) geoPackage;
 
 @end
--- geopackage-ios/dgiwg/GPKGDgiwgGeoPackageManager.m
+++ geopackage-ios/dgiwg/GPKGDgiwgGeoPackageManager.m
@@ -7,9 +7,110 @@
 //
 
 #import "GPKGDgiwgGeoPackageManager.h"
+#import "GPKGDgiwgConstants.h"
+#import "GPKGCrsWktExtension.h"
 
 @implementation GPKGDgiwgGeoPackageManager
 
-// TODO
+-(instancetype) init{
+    self = [super init];
+    return self;
+}
+
+-(GPKGDgiwgFile *) create: (NSString *) database withMetadata: (NSString *) metadata{
+    return [self create:database withURI:GPKG_DGIWG_DMF_DEFAULT_URI andMetadata:metadata];
+}
+
+-(GPKGDgiwgFile *) create: (NSString *) database withURI: (NSString *) uri andMetadata: (NSString *) metadata{
+    GPKGDgiwgFile *geoPackageFile = nil;
+    if([super create:database]){
+        geoPackageFile = [self createDGIWG:database withURI:uri andMetadata:metadata];
+    }
+    return geoPackageFile;
+}
+
+-(GPKGDgiwgFile *) create: (NSString *) database inDirectory: (NSString *) dbDirectory withMetadata: (NSString *) metadata{
+    return [self create:database inDirectory:dbDirectory withURI:GPKG_DGIWG_DMF_DEFAULT_URI andMetadata:metadata];
+}
+
+-(GPKGDgiwgFile *) create: (NSString *) database inDirectory: (NSString *) dbDirectory withURI: (NSString *) uri andMetadata: (NSString *) metadata{
+    GPKGDgiwgFile *geoPackageFile = nil;
+    if([super create:database inDirectory:dbDirectory]){
+        geoPackageFile = [self createDGIWG:database withURI:uri andMetadata:metadata];
+    }
+    return geoPackageFile;
+}
+
+/**
+ * Create the DGIWG GeoPackage file
+ *
+ * @param database database name
+ * @param file     GeoPackage file path
+ * @param uri      URI
+ * @param metadata metadata
+ * @return GeoPackage file
+ */
+-(GPKGDgiwgFile *) createDGIWG: (NSString *) database withURI: (NSString *) uri andMetadata: (NSString *) metadata{
+
+    GPKGDgiwgFile *geoPackageFile = nil;
+
+    GPKGDgiwgGeoPackage *geoPackage = [self open:database];
+    @try {
+        
+        if(geoPackage != nil){
+            
+            GPKGCrsWktExtension *wktExtension = [[GPKGCrsWktExtension alloc] initWithGeoPackage:geoPackage];
+            [wktExtension extensionCreateVersion:GPKG_CRS_WKT_V_1];
+
+            [geoPackage createGeoPackageDatasetMetadata:metadata withURI:uri];
+
+            geoPackageFile = [geoPackage file];
+            
+        }
+        
+    } @finally {
+        [geoPackage close];
+    }
+
+    return geoPackageFile;
+}
+
+-(GPKGDgiwgGeoPackage *) open: (NSString *) database{
+    return [self open:database andValidate:YES];
+}
+
+-(GPKGDgiwgGeoPackage *) open: (NSString *) database andValidate: (BOOL) validate{
+    
+    GPKGDgiwgGeoPackage *geoPackage = nil;
+    
+    GPKGGeoPackage *gp = [super open:database];
+    if(gp != nil){
+        
+        geoPackage = [[GPKGDgiwgGeoPackage alloc] initWithGeoPackage:gp];
+        
+        if(validate){
+            [GPKGDgiwgGeoPackageManager validate:geoPackage];
+        }
+        
+    }
+
+    return geoPackage;
+}
+
+-(BOOL) deleteDGIWG: (GPKGDgiwgFile *) file{
+    return [super delete:file.file];
+}
+
+-(BOOL) deleteDGIWG: (GPKGDgiwgFile *) file andFile: (BOOL) deleteFile{
+    return [super delete:file.file andFile:deleteFile];
+}
+
++(BOOL) isValid: (GPKGDgiwgGeoPackage *) geoPackage{
+    return [geoPackage isValid];
+}
+
++(GPKGDgiwgValidationErrors *) validate: (GPKGDgiwgGeoPackage *) geoPackage{
+    return [geoPackage validate];
+}
 
 @end
